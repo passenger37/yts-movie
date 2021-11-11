b1f634148dcec7fe0db8045845fb64c6aa5be38f
anand <anandsingh8448535943@gmail.com>
2021-11-11 11:29:10 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,13 +7,13 @@ class Footer extends React.Component{
     return(
       <div className="footer">
         <div className="footer-1">
-            <small>Akshat Mittal</small>
-            <small>akshatmittal.0007@gmail.com</small> <br/>
+            <small>Anand Singh</small>
+            <small>anandsigh7571970002@gmail.com</small> <br/>
             <small>"Still much more to do"</small>
         </div>
         <div className="footer-2">
-          <a href="https://linkedin.com/in/akshat-mittal-4604301b6" target="_blank"><i className="fa fa-linkedin"></i></a> <br/>
-          <a href="https://github.com/drifter089" target="_blank"><i className="fa fa-github"></i></a>
+          <a href="linkedin.com/in/anand-singh-766109174" target="_blank"><i className="fa fa-linkedin"></i></a> <br/>
+          <a href="https://github.com/passenger37" target="_blank"><i className="fa fa-github"></i></a>
         </div>
       </div>
     );
